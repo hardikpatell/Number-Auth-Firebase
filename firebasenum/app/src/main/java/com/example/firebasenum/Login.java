aecb4043c1306f827d1444bc6aac38107299dd68
hardikpatell <hardikpatel.std@gmail.com>
2019-11-25 21:35:58 +0530

    login register complate

--- firebasenum/app/src/main/java/com/example/firebasenum/Login.java
+++ firebasenum/app/src/main/java/com/example/firebasenum/Login.java
@@ -3,34 +3,41 @@ package com.example.firebasenum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
 
 public class Login extends AppCompatActivity {
 
     private EditText etNum;
-
+    private Button btnContinue;
+    private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         etNum=findViewById(R.id.l_etNum);
+        btnContinue=findViewById(R.id.l_btncontinue);
 
-        etNum.setOnClickListener(new View.OnClickListener() {
+        btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String number = etNum.getText().toString().trim();
-                if(number.isEmpty()){
+                if(number.isEmpty() || number.length()!=10){
                     etNum.setError("Enter Number");
                     etNum.requestFocus();
                     return;
 
                 }
-                String phoNenumber= "+91"+number;
+                String phoNenumber="+91"+number;
                 Intent intent=new Intent(getApplicationContext(),Otp.class);
                 intent.putExtra("phoneNumber",phoNenumber);
                 startActivity(intent);
+
             }
         });
     }
--- firebasenum/app/src/main/java/com/example/firebasenum/Otp.java
+++ firebasenum/app/src/main/java/com/example/firebasenum/Otp.java
@@ -1,13 +1,22 @@
 package com.example.firebasenum;
 
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
-import android.os.Bundle;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskExecutors;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
@@ -15,15 +24,65 @@ import java.util.concurrent.TimeUnit;
 
 public class Otp extends AppCompatActivity {
 
+    private String verificationId;
+    private FirebaseAuth mAuth;
+    private EditText etOtp;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_otp);
 
-        String phoneNumber =getIntent().getStringExtra("phoneNnmber");
+        setContentView(R.layout.activity_otp);
+        etOtp = findViewById(R.id.o_etOtp);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        String phoneNumber = getIntent().getStringExtra("phoneNumber");
+        sendVerificationCode(phoneNumber);
+        findViewById(R.id.o_btnContinue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String code = etOtp.getText().toString();
+                if (code.isEmpty() || code.length() < 6) {
+                    etOtp.setError("Enter OTP...");
+                    etOtp.requestFocus();
+                    return;
+                }
+                verifyCode(code);
+
+            }
+        });
+
     }
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private void sendotp(String number){
+
+    private void verifyCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential).
+                addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(getApplicationContext(), Welcome.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Enter Correct OTP", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+
+    private void sendVerificationCode(String number) {
         PhoneAuthProvider.getInstance().verifyPhoneNumber(
                 number,
                 60,
@@ -37,21 +96,29 @@ public class Otp extends AppCompatActivity {
 
 
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks
-            mCallBack =new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
         @Override
         public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
             super.onCodeSent(s, forceResendingToken);
+
+            verificationId = s;
+
         }
 
         @Override
         public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+
+            if (code != null) {
+                verifyCode(code);
+            }
 
         }
 
         @Override
         public void onVerificationFailed(FirebaseException e) {
-
+            startActivity(new Intent(getApplicationContext(), Register.class));
         }
     };
 
